b18c3339be87852aad654bf298d520408438186e
LornaMoringa <lorna.kirui@student.moringaschool.com>
2021-06-09 09:07:50 +0300

    Created class source

---
+++ app/models.py
@@ -0,0 +1,16 @@
+class Source:
+    
+    """
+    class to define the source of news
+    """
+
+    def __init__(self,id,name,description,url,category,language,country):
+
+        self.id = id
+        self.name = name
+        self.description = description
+        self.url = url
+        self.category = category
+        self.language = language
+        self.country = country
+
