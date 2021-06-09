7b7551e5daf2a630b1bb58fc659f0d3df2924612
LornaMoringa <lorna.kirui@student.moringaschool.com>
2021-06-09 09:16:35 +0300

    Created class Article

--- app/models.py
+++ app/models.py
@@ -14,3 +14,20 @@ class Source:
         self.language = language
         self.country = country
 
+
+class Article:
+
+    """
+    class to define the news article
+    """
+
+    def __init__(self,id,author,title,description,url,urlToImage,publishedAt,content):
+        self.id = id
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.urlToImage = urlToImage
+        self.publishedAt = publishedAt
+        self.content = content
+
